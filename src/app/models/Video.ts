7060f52d9f11b824f1b3169737007aa60851e8f3
Saumya Nayak <saumyo224@gmail.com>
2025-07-02 17:39:55 +0530

    Add Video model with schema and interface for video data

---
+++ src/app/models/Video.ts
@@ -0,0 +1,63 @@
+import mongoose, {Schema, models, model} from "mongoose";
+import { title } from "process";
+
+
+const VIDEO_DIMENSION = {
+    height : 1080,
+    width : 1920,
+} as const
+
+export interface IVideo{
+    _id?: mongoose.Types.ObjectId,
+    title: string,
+    description: string,
+    videoURL: string,
+    thumbnailURL: string,
+    controls: boolean,
+    transformation?: {
+        height: number,
+        width: number,
+        quality: number,
+    }
+}
+
+export const videoSchema = new Schema<IVideo>(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        videoURL: {
+            type: String,
+            required: true
+        },
+        thumbnailURL: {
+            type: String,
+            required: true
+        },
+        controls: {
+            type: Boolean,
+            default: true
+        },
+        transformation: {
+            height: {type: Number, default: VIDEO_DIMENSION.height},
+            width: {type: Number, default: VIDEO_DIMENSION.width},
+            quality: {
+                type: Number,
+                min: 1,
+                max: 100
+            }
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Video = models?.Video || model<IVideo>("Video", videoSchema)
+
+export default Video;
