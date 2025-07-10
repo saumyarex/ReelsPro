17ebeecbc123664f5e8135ef6d0d86b84e0c8927
Saumya Nayak <saumyo224@gmail.com>
2025-07-10 23:30:05 +0530

    Add video upload API with authentication and validation

---
+++ src/app/api/video/route.ts
@@ -0,0 +1,59 @@
+import { NextRequest, NextResponse } from "next/server";
+import DbConnect from "@/lib/DbConnect";
+import Video from "@/models/Video";
+import { IVideo } from "@/models/Video";
+import { getServerSession } from "next-auth/next"
+import { authOption } from "@/lib/autOptions";
+
+
+export async function POST(req: NextRequest) {
+
+    const session = await getServerSession(authOption);
+    console.log(session)
+    const user = session?.user;
+
+    if(!user){
+        NextResponse.json({success: false, message:"Unauthorized request. Please login!"}, {status: 401})
+    }
+
+    try {
+
+        const body : IVideo = await req.json();
+
+        if( !body.title || !body.description || !body.videoURL || !body.thumbnailURL){
+            NextResponse.json({success: false, message:"All fields are required"}, {status: 400})
+        }
+
+        DbConnect();
+
+        const videoData : IVideo = {
+            ...body,
+            controls: body?.controls || true,
+            transformation : {
+                height : 1080,
+                width : 1920,
+                quality : body.transformation?.quality ?? 100,
+            }
+        }
+
+        const newVideo = await Video.create(videoData)
+
+        if(!newVideo){
+            NextResponse.json({success: false, message:"Uploading failed"}, {status: 500})
+        }
+
+        return NextResponse.json({success: true, message:"Video uploaded successfully", videoURl : newVideo.videoURL}, {status: 201})
+    
+} catch (error) {
+    console.log("Error uploaing video : ", error)
+    if(error instanceof Error){
+        NextResponse.json({success: false, message: error.message}, {status: 500})
+    }else{
+        NextResponse.json({success: false, message: "Internal server error"}, {status: 500})
+    }
+}
+}
+
+
+
+
