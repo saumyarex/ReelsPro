18a519600266a395077f404c8ac1cf2eb599509a
Saumya Nayak <saumyo224@gmail.com>
2025-07-11 23:50:29 +0530

    Enhance NavBar with upload and profile menu functionality, including logout option

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,14 @@
 "use client";
 import React, { useState } from "react";
-import Image from "next/image";
 import { User, CirclePlus } from "lucide-react";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 function NavBar() {
   // general variables
   const [searchInput, setSearchInput] = useState("");
+  const [profileMenuDisaplay, setProfileMenuDisplay] = useState(false);
 
   const { data: session } = useSession();
 
@@ -30,11 +31,16 @@ function NavBar() {
     }
   };
 
+  const logout = async () => {
+    await signOut({ redirect: false });
+    router.push("/");
+  };
+
   return (
     <nav className="p-7 border-b-2 border-gray-200">
-      <ul className="flex sm:gap-10 justify-between">
+      <ul className="flex sm:flex-row flex-col sm:gap-5 sm:space-y-0 space-y-4 justify-between items-center">
         {/* Logo */}
-        <li className="sm:text-3xl text-2xl font-black ">REELSPRO</li>
+        <li className="text-2xl md:text-3xl font-black ">REELSPRO</li>
 
         {/* Serach box */}
         <li className="w-full ">
@@ -69,21 +75,48 @@ function NavBar() {
 
         {/* users buttons */}
         {session ? (
-          <li className="flex items-center gap-10">
+          <li className="sm:flex hidden items-center gap-10  ">
             <div>
-              <button className="btn btn-soft btn-primary">
-                <input type="file" />
-                <CirclePlus />
-              </button>
+              <Link href={"/upload"}>
+                <button className="btn btn-soft btn-primary dark:broder-1 dark:border-neutral-100">
+                  <CirclePlus />
+                  Upload
+                </button>
+              </Link>
             </div>
-            <div className="avatar avatar-placeholder">
-              <div className="bg-gray-400 hover:bg-gray-500 hover:cursor-pointer border-2 text-neutral-content w-12 rounded-full">
-                <User />
+            <div
+              className="relative"
+              onMouseEnter={() => setProfileMenuDisplay(true)}
+              onMouseLeave={() => setProfileMenuDisplay(false)}
+            >
+              <div className="avatar avatar-placeholder">
+                <div className="bg-gray-400 hover:bg-gray-500 hover:cursor-pointer border-2 text-neutral-content w-12 rounded-full">
+                  <User />
+                </div>
               </div>
+
+              {profileMenuDisaplay && (
+                <div className="h-20 absolute -right-5">
+                  <div className="bg-gray-950 border-1 border-gray-200 px-5 py-2 rounded-md w-full   mt-2 ">
+                    <Link href={"/profile"}>
+                      <button className="hover:cursor-pointer hover:text-gray-400">
+                        Profile
+                      </button>
+                    </Link>
+
+                    <button
+                      className="hover:cursor-pointer hover:text-gray-400"
+                      onClick={logout}
+                    >
+                      Logout
+                    </button>
+                  </div>
+                </div>
+              )}
             </div>
           </li>
         ) : (
-          <li className="flex gap-5 ">
+          <li className="sm:flex hidden gap-5 ">
             <button onClick={login} className="btn btn-soft btn-primary">
               Login
             </button>
