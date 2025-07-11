69f7ddb02f73f78d73af11bc075d2b700f15bda6
Saumya Nayak <saumyo224@gmail.com>
2025-07-11 17:14:40 +0530

    Refactor NavBar component and added login and register logic

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,25 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
-import { User } from "lucide-react";
+import { User, CirclePlus } from "lucide-react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 function NavBar() {
+  // general variables
   const [searchInput, setSearchInput] = useState("");
-  const session = true;
+
+  const { data: session } = useSession();
+
+  const router = useRouter();
+
+  const login = () => {
+    router.push("/login");
+  };
+
+  const register = () => {
+    router.push("/register");
+  };
 
   const searchReels = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -17,15 +31,15 @@ function NavBar() {
   };
 
   return (
-    <nav className="p-7">
-      <ul>
+    <nav className="p-7 border-b-2 border-gray-200">
+      <ul className="flex sm:gap-10 justify-between">
         {/* Logo */}
         <li className="sm:text-3xl text-2xl font-black ">REELSPRO</li>
 
         {/* Serach box */}
-        <li>
-          <form onSubmit={searchReels}>
-            <label className="input rounded-4xl">
+        <li className="w-full ">
+          <form onSubmit={searchReels} className="w-full flex justify-center">
+            <label className="input input-md rounded-4xl">
               <svg
                 className="h-[1em] opacity-50"
                 xmlns="http://www.w3.org/2000/svg"
@@ -45,7 +59,7 @@ function NavBar() {
               <input
                 value={searchInput}
                 type="search"
-                className="grow"
+                className="grow input-md"
                 placeholder="Search"
                 onChange={(e) => setSearchInput(e.target.value)}
               />
@@ -55,17 +69,27 @@ function NavBar() {
 
         {/* users buttons */}
         {session ? (
-          <li className="">
+          <li className="flex items-center gap-10">
+            <div>
+              <button className="btn btn-soft btn-primary">
+                <input type="file" />
+                <CirclePlus />
+              </button>
+            </div>
             <div className="avatar avatar-placeholder">
-              <div className="bg-gray-400 border-2 text-neutral-content w-12 rounded-full">
+              <div className="bg-gray-400 hover:bg-gray-500 hover:cursor-pointer border-2 text-neutral-content w-12 rounded-full">
                 <User />
               </div>
             </div>
           </li>
         ) : (
-          <li>
-            <button className="btn btn-soft btn-primary">Login</button>
-            <button className="btn btn-soft btn-primary">Register</button>
+          <li className="flex gap-5 ">
+            <button onClick={login} className="btn btn-soft btn-primary">
+              Login
+            </button>
+            <button onClick={register} className="btn btn-soft btn-primary">
+              Register
+            </button>
           </li>
         )}
       </ul>
