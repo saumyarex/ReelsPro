88bb2a45500fbffcd1f4eadf52f4220529d32e6c
Saumya Nayak <saumyo224@gmail.com>
2025-07-12 00:07:57 +0530

    Add MobileFootbar component to Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import MobileFootbar from "@/components/MobileFootbar";
 import NavBar from "@/components/NavBar";
 
 export default function Home() {
   return (
     <div>
       <NavBar />
+      <MobileFootbar />
     </div>
   );
 }
