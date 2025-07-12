d779c0f741c333742f1c35d79ebb1d00ea088b7d
Saumya Nayak <saumyo224@gmail.com>
2025-07-12 13:51:53 +0530

    Add profile page component and enhance NavBar with mobile menu functionality

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,6 +9,7 @@ function NavBar() {
   // general variables
   const [searchInput, setSearchInput] = useState("");
   const [profileMenuDisaplay, setProfileMenuDisplay] = useState(false);
+  const [mobileMenuActive, setMobileMenuActive] = useState(false);
 
   const { data: session } = useSession();
 
@@ -36,6 +37,9 @@ function NavBar() {
     router.push("/");
   };
 
+  const toggelMobileMenu = () => {
+    setMobileMenuActive(!mobileMenuActive);
+  };
   return (
     <nav className="p-7 border-b-2 border-gray-200">
       <ul className="flex sm:flex-row flex-col sm:gap-5 sm:space-y-0 space-y-4 justify-between items-center">
@@ -73,7 +77,7 @@ function NavBar() {
           </form>
         </li>
 
-        {/* users buttons */}
+        {/* desktop users buttons */}
         {session ? (
           <li className="sm:flex hidden items-center gap-10  ">
             <div>
@@ -97,7 +101,7 @@ function NavBar() {
 
               {profileMenuDisaplay && (
                 <div className="h-20 absolute -right-5">
-                  <div className="bg-gray-950 border-1 border-gray-200 px-5 py-2 rounded-md w-full   mt-2 ">
+                  <div className="bg-gray-950 border-1 border-gray-200 px-5 py-2 rounded-md w-full   mt-2 text-white">
                     <Link href={"/profile"}>
                       <button className="hover:cursor-pointer hover:text-gray-400">
                         Profile
@@ -125,6 +129,74 @@ function NavBar() {
             </button>
           </li>
         )}
+
+        {/* mobile users buttons */}
+        <div className="absolute top-5 px-5 flex flex-col  items-end  sm:hidden w-full">
+          <label className="btn btn-circle swap swap-rotate">
+            {/* this hidden checkbox controls the state */}
+            <input type="checkbox" onClick={toggelMobileMenu} />
+
+            {/* hamburger icon */}
+            <svg
+              className="swap-off fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              viewBox="0 0 512 512"
+            >
+              <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+            </svg>
+
+            {/* close icon */}
+            <svg
+              className="swap-on fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              viewBox="0 0 512 512"
+            >
+              <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+            </svg>
+          </label>
+          {session ? (
+            <>
+              {mobileMenuActive && (
+                <div className="flex flex-col mt-2 self-start text-lg w-full font-medium ">
+                  <Link href={"/profile"}>
+                    <button className=" bg-white dark:bg-[#0a0a0a] dark:active:text-gray-500 active:text-gray-500 w-full py-2 rounded-t border-b-1 border-gray-300">
+                      Profile
+                    </button>
+                  </Link>
+
+                  <button
+                    onClick={logout}
+                    className=" bg-white dark:bg-[#0a0a0a] dark:active:text-gray-500 active:text-gray-500 border-gray-500 w-full py-2 rounded-b"
+                  >
+                    Logout
+                  </button>
+                </div>
+              )}
+            </>
+          ) : (
+            <>
+              {mobileMenuActive && (
+                <div className="flex flex-col mt-2 self-start text-lg w-full font-medium ">
+                  <Link href={"/login"}>
+                    {" "}
+                    <button className=" bg-white dark:bg-[#0a0a0a] dark:active:text-gray-500 active:text-gray-500  w-full py-2 rounded-t border-b-1 border-gray-300">
+                      Login
+                    </button>
+                  </Link>
+                  <Link href={"/register"}>
+                    <button className=" bg-white dark:bg-[#0a0a0a] dark:active:text-gray-500 active:text-gray-500  border-gray-500 w-full py-2 rounded-b">
+                      Register
+                    </button>
+                  </Link>
+                </div>
+              )}
+            </>
+          )}
+        </div>
       </ul>
     </nav>
   );
