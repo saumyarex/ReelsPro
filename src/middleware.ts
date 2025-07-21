3f82fa392bb6e22487767eec66a33375d43b2784
Saumya Nayak <saumyo224@gmail.com>
2025-07-21 16:37:23 +0530

    Refactor middleware function to remove unused request parameter

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server"
 
 export default withAuth(
   // `withAuth` augments your `Request` with the user's token.
-  function middleware(req) {
+  function middleware() {
     return NextResponse.next();
   },
   {
