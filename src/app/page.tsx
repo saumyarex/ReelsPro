aeb0c9c0bbb59ad12b1293624731d4360bc2a216
Saumya Nayak <saumyo224@gmail.com>
2025-07-21 16:37:32 +0530

    Add Videos component to Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import MobileFootbar from "@/components/MobileFootbar";
 import NavBar from "@/components/NavBar";
+import Videos from "@/components/Videos";
 
 export default function Home() {
   return (
     <div>
       <NavBar />
+      <Videos />
       <MobileFootbar />
     </div>
   );
