f7916e6a77974d9a37f907a601d65c95cc0e34d3
Saumya Nayak <saumyo224@gmail.com>
2025-07-21 12:48:45 +0530

    Refactor middleware to enhance authentication handling and update path matching

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,34 @@
-export { default } from "next-auth/middleware"
+import { withAuth } from "next-auth/middleware"
+import { NextResponse } from "next/server"
 
-export const config = { matcher: ["/profile"] }
+export default withAuth(
+  // `withAuth` augments your `Request` with the user's token.
+  function middleware(req) {
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ token, req }) => {
+        const url = req.nextUrl;
+
+        // public paths
+        if(url.pathname === "/" || url.pathname.startsWith("/api/videos") || url.pathname.startsWith("/profile")){
+            return true;
+        }
+
+        //auth paths
+        if(url.pathname.startsWith('/register') || url.pathname.startsWith('/login') || url.pathname.startsWith('/api/auth')) {
+            return true;
+        }
+
+        return !!token;
+      }
+    },
+  },
+)
+
+export const config = { 
+    matcher: [
+        "/((?!_next/static|_next/image|favicon.ico|public/).*)"
+    ] 
+};
