d7191caae07e43fe86db50d730ccd51380992583
Saumya Nayak <saumyo224@gmail.com>
2025-08-19 18:24:19 +0530

    Refactor layout to include NavBar within AuthProvider and remove NavBar from Home component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "../../providers/AuthProvider";
+import NavBar from "@/components/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthProvider>{children}</AuthProvider>
+        <AuthProvider>
+          <NavBar />
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import MobileFootbar from "@/components/MobileFootbar";
-import NavBar from "@/components/NavBar";
 import Videos from "@/components/Videos";
 
 export default function Home() {
   return (
     <div>
-      <NavBar />
       <Videos />
       <MobileFootbar />
     </div>
