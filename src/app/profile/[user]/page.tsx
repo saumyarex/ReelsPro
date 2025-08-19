3e423f69ff20017bedb327eb34e6539014f8c6bb
Saumya Nayak <saumyo224@gmail.com>
2025-08-19 18:24:52 +0530

    Refactor Profile component to include userId extraction and improve layout structure

--- src/app/profile/[user]/page.tsx
+++ src/app/profile/[user]/page.tsx
@@ -2,14 +2,15 @@
 import React from "react";
 import { useParams } from "next/navigation";
 import Videos from "@/components/Videos";
+import { EllipsisVertical } from "lucide-react";
+
 function Profile() {
   const { user } = useParams();
+  const userId = Array.isArray(user) ? user[0] : user;
   return (
-    <div className="flex flex-col items-center p-5">
-      <h1 className="text-3xl border-b-2 w-full text-center pb-5 border-gray-500">
-        {user}
-      </h1>
-      <Videos></Videos>
+    <div className="flex flex-col min-h-screen items-center p-5">
+      <Videos userId={userId}></Videos>
+      <div></div>
     </div>
   );
 }
