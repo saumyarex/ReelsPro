41766ff99401d706a42a7fadf74a6d0d13549fd9
Saumya Nayak <saumyo224@gmail.com>
2025-08-20 16:38:55 +0530

    Remove MobileFootbar component from Home page to streamline layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import MobileFootbar from "@/components/MobileFootbar";
 import Videos from "@/components/Videos";
 
 export default function Home() {
   return (
     <div>
       <Videos />
-      <MobileFootbar />
     </div>
   );
 }
